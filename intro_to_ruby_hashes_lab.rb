916c3eb98a4db07c5bdf4d6b308974a8f5fa5042
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-09 23:12:22 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,11 +24,11 @@ def monopoly
 	        "mortgage_value" = "$800"
 	      }
 	    }
-	    rent_in_dollars: {
-	      one_piece_owned: 25
-	      two_pieces_owned: 50
-	      three_pieces_owned: 100
-	      four_pieces_owned: 200
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25
+	      :two_pieces_owned => 50
+	      :three_pieces_owned => 100
+	      :four_pieces_owned => 200
 	    }
 	  }
 	}
