2100927cba49012df37777009d96bca30c244b67
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-09 23:05:27 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly
 	      reading_railroad: {}
 	      pennsylvania_railroad: {}
 	      b_and_o_railroad: {}
-	      
+	      shortline: {}
 	    }
 	    rent_in_dollars: = {
 	      one_piece_owned: 25
